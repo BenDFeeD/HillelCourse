3470479eca8405a28a22054c2cd2fd8ced1dbfc4
BenD <bendfeed@gmail.com>
2023-09-07 17:10:33 +0300

    #

---
+++ Lesson_3/Task_3.5/Program.cs
@@ -0,0 +1,39 @@
+﻿double ReadNumberFromConsole(string message)
+{
+    Console.WriteLine(message);
+    string input = Console.ReadLine();
+    bool resParse = double.TryParse(input, out double result);
+    if (!resParse)
+    {
+        Console.WriteLine("Error");
+        return 0;
+    }
+    return result;
+}
+    while (true)
+    {
+        double num = ReadNumberFromConsole("Enter your number:");
+    if (1 <= num & num <= 100)
+    {
+        if ((num % 3 == 0) & (num % 5 != 0) & (num %15 != 0))
+        {
+            Console.WriteLine("Fizz");
+        }
+        else if ((num % 5 == 0) & (num % 15 != 0) & (num % 3 != 0))
+        {
+            Console.WriteLine("Buzz");
+        }
+        else if (num % 15 == 0)
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+        else
+        {
+            Console.WriteLine("Your number is not multiple 3, 5, 15");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Your number is bigger than 100, or less 1. Try again.");
+    }
+}
